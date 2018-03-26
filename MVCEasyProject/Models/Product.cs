9c61eb2663c9dcd1d64f167faa16e44f97d04740
Sergiy <slevin.uk@gmail.com>
2018-03-26 20:36:43 +0300

    update product

--- MVCEasyProject/Models/Product.cs
+++ MVCEasyProject/Models/Product.cs
@@ -10,11 +10,12 @@ namespace MVCEasyProject.Models
     {
         [Key]
         public int Id { get; set; }
-        [Required(ErrorMessage = "Enter new product name123")]
+        [Required(ErrorMessage = "Enter new product name1")]
         public string Name { get; set; }
         [Required(ErrorMessage = "Enter product company1")]
         public string Company { get; set; }
         public string Description { get; set; }
+        [Required(ErrorMessage = "Enter correct price")]
         public decimal Price { get; set; }
         public bool InStock { get; set; }
         public int? CategoryId { get; set; }
